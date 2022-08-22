f3e2c629a12b5b3301ad2ebd1fac3f0f2a200c96
Threshold247 <65241733+Threshold247@users.noreply.github.com>
2022-08-22 17:09:58 +0200

    Completed Coffee machine OOP

--- Udemy/Coffee Machine OOP/main.py
+++ Udemy/Coffee Machine OOP/main.py
@@ -3,3 +3,29 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 
+my_money = MoneyMachine()
+my_coffee = CoffeeMaker()
+my_menu = Menu()
+
+is_on = True
+
+
+while is_on:
+
+    choice = input(f"Choose a beverage:{my_menu.get_items()}\n")
+
+    if choice == "off":
+        print("Shutting down")
+        is_on = False
+    elif choice == "report":
+        my_coffee.report()
+        my_money.report()
+    else:
+        drink = my_menu.find_drink(choice)
+        if my_coffee.is_resource_sufficient(drink) and my_money.make_payment(drink.cost):
+            my_coffee.make_coffee(drink)
+
+
+
+
+
