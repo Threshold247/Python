ee691e907b1c772efc35339c852da76903c4b7cb
Threshold247 <65241733+Threshold247@users.noreply.github.com>
2022-08-23 12:52:10 +0200

    Practice OOP Created the Quiz show

--- Udemy/OOP/main.py
+++ Udemy/OOP/main.py
@@ -14,11 +14,32 @@
 #
 # my_screen.exitonclick()
 
-from prettytable import PrettyTable
+# from prettytable import PrettyTable
 
-table = PrettyTable()
-table.add_column("Pokemon name",["Pikachu","Squirtle","Charmander"]), table.add_column("Type",["Electric","Water","Fire"])
+# table = PrettyTable()
+# table.add_column("Pokemon name",["Pikachu","Squirtle","Charmander"]), table.add_column("Type",["Electric","Water","Fire"])
 
-table.align ="l"
+# table.align ="l"
 
-print(table)
+# print(table)
+
+class User:
+
+    def __init__(self, id, user):
+        self.user_id = id
+        self.user_name = user
+        self.followers = 0
+        print(f"constructed {id} {user} has {self.followers} followers")
+
+    def add_followers(self):
+        self.followers += 1
+        print(f"{self.user_id} {self.user_name} now has {self.followers} followers")
+
+user_1 = User(1, "Jimbo")
+user_2 = User(2, "Jill")
+
+user_1.add_followers()
+user_2.add_followers()
+
+
+
