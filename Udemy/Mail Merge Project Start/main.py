79051a0cb43a2f07faa01408f60b7cfbbdd3d486
Threshold247 <65241733+Threshold247@users.noreply.github.com>
2022-11-16 11:03:39 +0200

    Created mail Merge

--- Udemy/Mail Merge Project Start/main.py
+++ Udemy/Mail Merge Project Start/main.py
@@ -1,8 +1,24 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
+# TODO: Create a letter using starting_letter.txt
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
     
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open('./Input/Letters/starting_letter.txt') as file:
+    starting_letter = file.read()
+
+with open('./Input/Names/invited_names.txt') as file:
+    # created a list of names
+    invited_names = file.readlines()
+    # loop through the list to get each name
+    for name in invited_names:
+        # strip away any white space
+        strip_name = name.strip()
+        # replace the name with the new name from invited name list
+        new_letter = starting_letter.replace('[name]', strip_name)
+        print(new_letter)
+        with open('./Output/ReadyToSend/letters.txt', mode="w") as file1:
+            file1.write(new_letter)
