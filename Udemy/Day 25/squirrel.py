41ee73ab970783d162c746145f130019d3f898e8
Threshold247 <65241733+Threshold247@users.noreply.github.com>
2022-11-30 14:58:30 +0200

    created a squirrel fur data

--- Udemy/Day 25/squirrel.py
+++ Udemy/Day 25/squirrel.py
@@ -1,2 +1,20 @@
+import pandas as pd
 
+# reading the data from the csv file
+squirrel_data = pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
 
+# creating fur data variable with only fur color series
+fur_data = squirrel_data["Primary Fur Color"]
+# creating
+gray_fur_data = len(squirrel_data[squirrel_data["Primary Fur Color"] == "Gray"])
+red_fur_data = len(squirrel_data[squirrel_data["Primary Fur Color"] == "Cinnamon"])
+black_fur_data = len(squirrel_data[squirrel_data["Primary Fur Color"] == "Black"])
+print(gray_fur_data, red_fur_data, black_fur_data)
+
+squirrel_fur_data = {
+    "Fur Colour": ["Gray", "Red", "Black"],
+    "Count": [gray_fur_data, red_fur_data, black_fur_data]
+}
+
+data = pd.DataFrame(squirrel_fur_data)
+data.to_csv("Squirrel_Color_Data")
