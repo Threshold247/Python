3e4e09c92740d8b2547b1f7c133519d1e382c84a
Threshold247 <65241733+Threshold247@users.noreply.github.com>
2022-12-02 11:42:36 +0200

    Checked through data in series. Search is not case-sensitive. Extracted from series Inserted data from series in to turtle position

--- Udemy/US States Game/data.py
+++ Udemy/US States Game/data.py
@@ -1,19 +1,23 @@
+import re
+
 import pandas as pd
 
 data = pd.read_csv("50_states.csv")
 
-state = data["state"]
-x_cord = data["x"]
-y_cord = data["y"]
+# capitalize the first letter of each word
+test = input("Enter State:").lower()
+# check if in input is in the Series. returns True or False
+check = data["state"].str.findall(test, flags=re.IGNORECASE)
 
-
-test = input("Enter State:")
-check = test in data["state"].values
-if check:
-    answer = (data[data["state"] == test])
-    print(answer)
-    answer_x_cord = answer["x"]
-    answer_y_cord = answer["y"]
+for item in check:
+    if len(item) > 0:
+        # Search through DataFrame if there is a state that matches the input
+        answer = (data[data["state"] == item[0]])
+        print(answer)
+        test_answer = answer.values
+        answer_list = test_answer[0]
+        answer_x_cord = int(answer_list[1])
+        answer_y_cord = int(answer_list[2])
 
 
 
--- Udemy/US States Game/main.py
+++ Udemy/US States Game/main.py
@@ -1,7 +1,10 @@
 import turtle
-from data import data
+import pandas as pd
+import re
 
+data = pd.read_csv("50_states.csv")
 
+second_turtle = turtle.Turtle()
 screen = turtle.Screen()
 screen.title("Guess the State")
 # change shape to custom shape
@@ -10,16 +13,32 @@ screen.addshape(image)
 
 turtle.shape(image)
 
-# game_on = True
+game_on = True
 
-# while game_on:
+while game_on:
 
-
-user_answer = screen.textinput(title="Guess the State", prompt="Can you guess another State")
-print(user_answer)
-if data["state"] == user_answer:
-    print(True)
-
+    user_answer = screen.textinput(title="Guess the State", prompt="Can you guess another State")
+    check = data["state"].str.findall(user_answer, flags=re.IGNORECASE)
+    # if check returns True
+    for name in check:
+        if len(name) > 0:
+            # Search through DataFrame if there is a state that matches the input
+            answer = (data[data["state"] == name[0]])
+            print(f'original answer: {answer}')
+            # returns actual values in series
+            answer = answer.values
+            # navigate through list by indexing
+            print(f'converted answer: {answer}')
+            answer_list = answer[0]
+            print(f'answer in list: {answer_list}')
+            # convert value into integer
+            state_name = answer_list[0]
+            state_x_cord = float(answer_list[1])
+            state_y_cord = float(answer_list[2])
+            second_turtle.pu()
+            second_turtle.hideturtle()
+            second_turtle.goto(state_x_cord, state_y_cord)
+            second_turtle.write(state_name, align="center")
 # keeps the screen active
 turtle.mainloop()
 
