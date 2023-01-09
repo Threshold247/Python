173af43e1802e38398b8df04e956d07a132643fc
Threshold247 <65241733+Threshold247@users.noreply.github.com>
2023-01-09 11:09:42 +0200

    Updated notes for exercises

--- Udemy/Exercises/dictionary comprehension.py
+++ Udemy/Exercises/dictionary comprehension.py
@@ -2,16 +2,18 @@ from random import randint
 
 names = ["Alex", "Beth", "Caroline", "Eleanor", "Dave", "Freddie"]
 
+# creates a dictionary which consists of student name (key) and a random score (value)
 students_score = {student: randint(1, 99) for student in names}
 print(students_score)
 
+# creates a dictionary which consists of student name and score where score is higher than 60
 students_passed = {key: value for (key, value) in students_score.items() if value > 60}
 print(students_passed)
 
 sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
-
+# creates a dictionary that splits a sentence into words(key) and counts the letter for each word (value)
 result = {item: len(item) for item in sentence.split()}
-print(result)
+print(f"Split sentence with count: {result}")
 
 weather_c = {
     "Monday": 12,
@@ -22,6 +24,8 @@ weather_c = {
     "Saturday": 22,
     "Sunday": 24,
 }
+
 # (temp_c * 9/5) + 32 = temp_f
+# creates a dictionary which consists of the day (key) and converted temperature (value)
 weather_f = {key: ((values*9/5) + 32) for (key, values) in weather_c.items()}
-print(weather_f)
+print(f"Converted weather {weather_f}")
