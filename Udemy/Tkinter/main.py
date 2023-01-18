c4058f2890127d94fa6aa3dd594dc70498762baa
Threshold247 <65241733+Threshold247@users.noreply.github.com>
2023-01-18 09:59:23 +0200

    updated notes for tkinter

--- Udemy/Tkinter/main.py
+++ Udemy/Tkinter/main.py
@@ -40,6 +40,33 @@ my_button.pack()
 my_input = Entry()
 my_input.pack()
 
+# single line entry box
+
+my_entry = Entry(width=30)
+# insert text into the box
+my_entry.insert(END, string=" A text box")
+print(my_entry.get())
+my_entry.pack()
+
+# text box
+
+my_textbox = Text(width=50, height=8)
+# place cursor in box
+my_textbox.focus()
+my_textbox.insert(END, " A multi line text box")
+# get the text from the multi line box
+print(my_textbox.get("1.0", END))
+my_textbox.pack()
+
+# Create a spinbox
+
+
+def spin_input():
+    print(my_spinbox.get())
+
+
+my_spinbox = Spinbox(from_=0, to=10, width= 10, command=spin_input)
+my_spinbox.pack()
 
 # remains at the very end of the program to continue to display window
 window.mainloop()
