c56f5f041111ae87368f7ad5b5b2170e6acc0606
Threshold247 <65241733+Threshold247@users.noreply.github.com>
2023-02-01 09:12:03 +0200

    Added Timer label Added start button Added reset button Added check mark

--- Udemy/Pomodorro/main.py
+++ Udemy/Pomodorro/main.py
@@ -19,18 +19,29 @@ LONG_BREAK_MIN = 20
 window = Tk()
 window.title("Pomodoro Timer")
 window.config(padx=80, pady=50, bg=YELLOW)
+text = "✔"
 
 # setup background image
-canvas = Canvas(width=200, height=224,bg=YELLOW,highlightthickness=0)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 # store image in variable
 tomato_image = PhotoImage(file="tomato.png")
 # create canvas (x-cord, y=cord, image reference
 canvas.create_image(100, 112, image=tomato_image)
 # create text on image (x-cord, y-cord, text, text color, font(font type, font size, bold/italic))
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
-canvas.pack()
+canvas.grid(row=1, column=1)
 
+my_label = Label(text="Timer", font=(FONT_NAME, 30, "bold"), bg=YELLOW, fg=GREEN)
+my_label.grid(row=0, column=1)
 
+start_button = Button(text="Start", font=(FONT_NAME, 10, "bold"))
+start_button.grid(row=2, column=0)
+
+checkmark_label = Label(text=text, bg=YELLOW, fg=GREEN)
+checkmark_label.grid(row=3, column=1)
+
+reset_button = Button(text="Reset", font=(FONT_NAME, 10, "bold"))
+reset_button.grid(row=2, column=2)
 
 window.mainloop()
 
