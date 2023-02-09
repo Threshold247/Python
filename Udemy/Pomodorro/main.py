8b78040679fc5f7a178f00f7977e4d276bb3eb35
Threshold247 <65241733+Threshold247@users.noreply.github.com>
2023-02-09 20:14:07 +0200

    Added check box markers to show after each work session Added reset timer functionality

--- Udemy/Pomodorro/main.py
+++ Udemy/Pomodorro/main.py
@@ -6,14 +6,22 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 2
-SHORT_BREAK_MIN = 1
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer = None
 
-# ---------------------------- TIMER RESET ------------------------------- # 
-# def reset_timer():
-#     canvas.itemconfig(timer_text, text="00:00")
+# ---------------------------- TIMER RESET ------------------------------- #
+
+
+def reset_timer():
+    global reps
+    reps = 0
+    window.after_cancel(timer)
+    my_label.config(text="Timer")
+    checkmark_label.config(text="")
+    canvas.itemconfig(timer_text, text="00:00")
 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
@@ -24,19 +32,22 @@ def start_count():
     short_break_sec = SHORT_BREAK_MIN*60
     long_break_sec = LONG_BREAK_MIN*60
     reps += 1
+
     if reps % 8 == 0:
         countdown(long_break_sec)
+        my_label.config(text="L.Break", fg=RED)
     elif reps % 2 == 0:
-        print(reps)
         countdown(short_break_sec)
+        my_label.config(text="S.Break", fg=PINK)
+        print(reps)
     else:
         countdown(work_seconds)
-
-
-
+        my_label.config(text="Work", fg=GREEN)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+
 def countdown(count):
     # convert into minutes rounding down
     count_minute = math.floor(count/60)
@@ -48,10 +59,16 @@ def countdown(count):
     canvas.itemconfig(timer_text, text=f"{count_minute}:{count_second}")
     # starting the timer
     if count > 0:
-        window.after(100, countdown, count-1)
-    # resetting the timer
+        global timer
+        timer = window.after(1000, countdown, count-1)
     else:
         start_count()
+        marks = ""
+        work_session = math.floor(reps/2)
+        for _ in range(work_session):
+            marks += text
+            checkmark_label.config(text=marks)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -78,10 +95,10 @@ my_label.grid(row=0, column=1)
 start_button = Button(text="Start", font=(FONT_NAME, 8, "bold"), highlightthickness=0, command=start_count)
 start_button.grid(row=2, column=0)
 
-checkmark_label = Label(text=text, bg=YELLOW, fg=GREEN)
+checkmark_label = Label(bg=YELLOW, fg=GREEN)
 checkmark_label.grid(row=3, column=1)
 
-reset_button = Button(text="Reset", font=(FONT_NAME, 8, "bold"), highlightthickness=0) # command=reset_timer)
+reset_button = Button(text="Reset", font=(FONT_NAME, 8, "bold"), highlightthickness=0, command=reset_timer)
 reset_button.grid(row=2, column=2)
 
 window.mainloop()
