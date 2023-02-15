8a360e217b7b9f8bc14be849cdc188b1a8b96b9b
Threshold247 <65241733+Threshold247@users.noreply.github.com>
2023-02-15 14:22:59 +0200

    Added try and except Added line to re-run function

--- Udemy/NATO-alphabet-start/main.py
+++ Udemy/NATO-alphabet-start/main.py
@@ -8,11 +8,22 @@ data = pd.read_csv("nato_phonetic_alphabet.csv")
 
 nato_alphabet = {index: row for (index, row) in data.iterrows()}
 test = {row.letter: row.code for row in nato_alphabet.values()}
+print(test)
 
-# Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Enter a word:").upper()
 
-result = [test[letter] for letter in user_input]
-print(result)
+def generate_alphabet():
+    # Create a list of the phonetic code words from a word that the user inputs.
+    user_input = input("Enter a word:").upper()
+    try:
+        result = [test[letter] for letter in user_input]
+    except KeyError:
+        print("Please enter a letter")
+    # re-run the function
+        generate_alphabet()
+    else:
+        print(result)
+
+
+generate_alphabet()
 
 
