e722419a95fb288b1e603e0af19882e6c530d12b
Threshold247 <65241733+Threshold247@users.noreply.github.com>
2023-03-07 09:06:37 +0200

    Set up Flash cards Added correct and incorrect button Mapped function to correct and incorrect button Generate random word when correct or incorrect button is pressed.

--- Udemy/Flash Cards/main.py
+++ Udemy/Flash Cards/main.py
@@ -1,6 +1,20 @@
 from tkinter import *
+import pandas as pd
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+# Read data
+data = pd.read_csv("data/french_words.csv")
+# Convert dataframe to list of dictionaries
+test = data.to_dict(orient="records")
+
+# ------ Random word -----#
+def random_word():
+    french_word = (test[random.randrange(0, 102)]['French'])
+    canvas.itemconfig(card_title, text=f"French")
+    canvas.itemconfig(card_word, text=f"{french_word}")
+
 
 window = Tk()
 window.title("Flash Cards")
@@ -11,21 +25,22 @@ canvas = Canvas(width=800, height=526)
 card_front_image = PhotoImage(file="images/card_front.png")
 # create image with x and y co-ordinate
 canvas.create_image(400, 263, image=card_front_image)
-# create text with x and y co-ordinate, text, font ty[e, size, style
-canvas.create_text(400, 150, text="Text", font=("Arial", 40, "italic"))
-canvas.create_text(400, 263, text="Word", font=("Arial", 60, "bold"))
+# create text with x and y co-ordinate, text, font ty[e, size, style and assign to variable in order to edit
+card_title = canvas.create_text(400, 150, text="", font=("Arial", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Arial", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
 # Buttons
 correct_image = PhotoImage(file="images/right.png")
-known_btn = Button(image=correct_image, highlightthickness=0)
+known_btn = Button(image=correct_image, highlightthickness=0, command=random_word)
 known_btn.grid(row=1, column=1)
 
 incorrect_image = PhotoImage(file="images/wrong.png")
-unknown_btn = Button(image=incorrect_image, highlightthickness=0)
+unknown_btn = Button(image=incorrect_image, highlightthickness=0, command=random_word)
 unknown_btn.grid(row=1, column=0)
 
+random_word()
 
 
 window.mainloop()
