813f6f4834424aca33128ab0d2d69cc357582eac
Threshold247 <65241733+Threshold247@users.noreply.github.com>
2023-07-18 09:37:22 +0200

    Setup Task app. Created window from Tkinter

---
+++ To do app/main.py
@@ -0,0 +1,14 @@
+# create Task app
+# front end to add a task, displays the task added, able to delete a task.
+# task has a description, date
+# create an Api
+# connect to a database (internal port 8000)
+from tkinter import *
+
+window = Tk()
+window.title("Task app")
+window.config(padx=50, pady=50)
+# create canvas
+canvas = Canvas(width=800, height=526)
+
+window.mainloop()
