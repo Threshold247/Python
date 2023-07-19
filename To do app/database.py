624eee1ca91847e1b2debbbabdb54015cf37f197
Threshold247 <65241733+Threshold247@users.noreply.github.com>
2023-07-19 14:47:01 +0200

    Added Select SQL instruction to read from the database.

--- To do app/database.py
+++ To do app/database.py
@@ -1,13 +1,24 @@
 import psycopg2
 import os
 from dotenv import load_dotenv
-load_dotenv()
 
-connection = psycopg2.connect(
-    host="localhost",
-    database="suppliers",
-    user="postgres",
-    password="Abcd1234"
-)
+load_dotenv(".env")
+params = {
+    "host": os.getenv("HOST"),
+    "database": os.getenv("DATABASE"),
+    "user": os.getenv("USER"),
+    "password": os.getenv("PASSWORD")
+    }
+sql = '''SELECT * FROM tasks'''
+connection = psycopg2.connect(**params)
 
+# reading data from the database
+cur = connection.cursor()
+cur.execute(sql)
+# print the number of rows
+print(cur.rowcount)
+# print the data of each row
+row = cur.fetchone()
+print(row)
+cur.close()
 
