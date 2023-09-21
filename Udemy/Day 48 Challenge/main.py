52d2e0db47832de7f64e48050b95fd4a0066006e
Threshold247 <65241733+Threshold247@users.noreply.github.com>
2023-09-21 16:00:02 +0200

    Added new notes per code line

--- Udemy/Day 48 Challenge/main.py
+++ Udemy/Day 48 Challenge/main.py
@@ -9,24 +9,24 @@ driver = webdriver.Firefox(
 python_url = "https://python.org"
 
 driver.get(url=python_url)
-upcoming_events = driver.find_elements(By.XPATH, value="/html/body/div/div[3]/div/section/div[2]/div[2]/div/ul/li")
 
-my_time = []
-my_name = []
-for item in upcoming_events:
-    test_split = item.text.split("\n", maxsplit=1)
-    my_time.append(test_split[0])
-    my_name.append(test_split[1])
-
-my_dict = dict(zip(my_time, my_name))
+# get a list of the event date text
+event_date = [item.text for item in driver.find_elements(By.XPATH, value="/html/body/div/div[3]/div/section/div[2]/"
+                                                                         "div[2]/div/ul/li/time")]
+# get a list of the event description text
+event_name = [item.text for item in driver.find_elements(By.XPATH, value="/html/body/div/div[3]/div/section/div[2]/"
+                                                                         "div[2]/div/ul/li/a")]
+# combine the lists and create a dictionary
+my_dict = dict(zip(event_name, event_date))
 
 events = {}
 count = -1
 for (key, value) in my_dict.items():
-    count+=1
+    count += 1
+    # create a dictionary with the key is the count+1 and the value is the key,value pair of my_dict
     events[count] = {
-        "time": key,
-        "name": value
+        "time": value,
+        "name": key
     }
 print(events)
 
