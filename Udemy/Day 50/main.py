a6f17e65ff5e9e16c1bef7b660dde135c092c7bf
Threshold247 <65241733+Threshold247@users.noreply.github.com>
2023-10-30 19:49:59 +0200

    Login with Google button Click on Login with Google button Switch between 2 windows. Enter google email address Click on the next button

--- Udemy/Day 50/main.py
+++ Udemy/Day 50/main.py
@@ -13,21 +13,30 @@ driver = webdriver.Firefox(
 t_url = "https://www.tinder.com"
 
 driver.get(url=t_url)
+# wait 3 seconds for window to render
 time.sleep(3)
+# find accept button
 accept_btn = driver.find_element(By.XPATH, value="/html/body/div[1]/div/div[2]/div/div/div[1]/div[1]/button")
+# click on accept cookies button
 accept_btn.click()
 
+# find login button
 login = driver.find_element(By.XPATH, value="/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/"
                                             "div[2]/div[2]/a")
+# click on login button
 login.click()
+# wait 5 seconds for window to render
 time.sleep(5)
 
-
+# click on google button
 try:
     Google_button = driver.find_element(By.XPATH, value='/html/body/div[2]/main/div/div/div[1]/div/div/div[2]'
                                                         '/div[2]/span/div[1]/div')
+    # click on Google button
     Google_button.click()
+    # wait 5 seconds for window to render
     time.sleep(5)
+# if the element is not found
 except NoSuchElementException:
     print("Error")
 
