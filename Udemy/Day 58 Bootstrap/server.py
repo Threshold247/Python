b07222ed9a9fe813d252ecefaff9f53897a4bd00
Threshold247 <65241733+Threshold247@users.noreply.github.com>
2024-01-16 13:30:33 +0200

    Learning how to use Bootstrap components

--- Udemy/Day 58 Bootstrap/server.py
+++ Udemy/Day 58 Bootstrap/server.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
-
+from datetime import datetime
 
 app = Flask(__name__)
+
+current_year = datetime.now().year
+print(current_year)
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+
+    return render_template('test.html', current_year=current_year)
 
 
 if __name__ == '__main__':
