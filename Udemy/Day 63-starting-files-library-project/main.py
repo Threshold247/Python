0743f800320d1a5ba5ee2235ec4254c36ef46c4a
Threshold247 <65241733+Threshold247@users.noreply.github.com>
2024-01-30 14:26:43 +0200

    Setting up Library website to display and add books from a database

---
+++ Udemy/Day 63-starting-files-library-project/main.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+
+
+'''
+Red underlines? Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+
+app = Flask(__name__)
+
+all_books = []
+
+
+@app.route('/')
+def home():
+    return render_template('index.html', books=all_books)
+
+
+@app.route("/add", methods=['GET', 'POST'])
+def add():
+    if request.method == "POST":
+        book_dict = {
+            "title": request.form.get('title'),
+            "author": request.form.get('author'),
+            "rating": request.form.get('rating')
+        }
+        all_books.append(book_dict)
+        return redirect(url_for('home'))
+
+    return render_template('add.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
