7537c143d1092daaed921e7da895c512abdcca24
Threshold247 <65241733+Threshold247@users.noreply.github.com>
2024-02-25 09:47:03 +0200

    Capstone project. Rendering a blog post site that allows a user to add and edit blog posts. added routes in main.py to -request all posts -select a single post and display on a new page -add a new post

---
+++ Udemy/Day 67-starting-files-upgraded-blog/main.py
@@ -0,0 +1,120 @@
+from flask import Flask, render_template, redirect, url_for,request,jsonify
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Text
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
+from datetime import date
+
+'''
+Make sure the required packages are installed: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from the requirements.txt for this project.
+'''
+
+app = Flask(__name__)
+app.config['CKEDITOR_PKG_TYPE'] = 'basic'
+ckeditor = CKEditor(app)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+
+# CREATE DATABASE
+class Base(DeclarativeBase):
+    pass
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+# CONFIGURE TABLE
+class BlogPost(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
+    date: Mapped[str] = mapped_column(String(250), nullable=False)
+    body: Mapped[str] = mapped_column(Text, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+# Create form
+
+class MyForm(FlaskForm):
+    # adding a validators requires a valid input which will prompt user to fill out the input.
+    title = StringField(label='Title', validators=[DataRequired()])
+    subtitle = StringField(label='Subtitle', validators=[DataRequired()])
+    date = StringField(label='Date', validators=[DataRequired()])
+    # requires CKEditor because of the amount of text required
+    body = CKEditorField(label='Body', validators=[DataRequired()])
+    author = StringField(label='Author', validators=[DataRequired()])
+    img_url = StringField(label='Image URL', validators=[URL()])
+    submit_btn = SubmitField(label="Submit")
+
+
+@app.route('/')
+def get_all_posts():
+    # Query the database for all the posts. Convert the data to a python list.
+    posts = db.session.execute(db.select(BlogPost).order_by(BlogPost.date)).scalars().all()
+    return render_template("index.html", all_posts=posts)
+
+
+# Add a route so that you can click on individual posts.
+@app.route('/single/<int:post_id>', methods=['GET', 'POST'])
+def show_post(post_id):
+    # Retrieve a BlogPost from the database based on the post_id
+    requested_post = db.get_or_404(BlogPost, post_id)
+    return render_template("post.html", post=requested_post)
+
+
+# TODO: add_new_post() to create a new blog post
+@app.route('/make_post', methods=['GET', 'POST'])
+def add_new_post():
+    form = MyForm()
+
+    form.validate_on_submit()
+    # try:
+    #     if request.method == 'POST':
+    #         add_post = BlogPost(
+    #             id=request.form.get('id'),
+    #
+    #         )
+    #         db.session.add(add_post)
+    #         db.session.commit()
+    #         return render_template('make-post.html')
+    # except Exception as error:
+    #     return jsonify({"error": {"message": str(error)}}), 500
+    return render_template('make-post.html', form=form)
+
+
+# TODO: edit_post() to change an existing blog post
+
+# TODO: delete_post() to remove a blog post from the database
+
+# Below is the code from previous lessons. No changes needed.
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5003)
